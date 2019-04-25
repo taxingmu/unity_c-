ac1f4b0ddac33d4fe333f1aa902797533d8bbf10
taxingmu <664300829@qq,com>
2019-04-25 21:45:19 +0800

    原型模式，主要依靠浅拷贝，返回浅拷贝

---
+++ Prototype/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    //产品继承自产品原型，主要使用一个api进行浅拷贝
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorManager colormanager = new ColorManager();
+
+            // Initialize with standard colors
+            colormanager["red"] = new Color(255, 0, 0);
+            colormanager["green"] = new Color(0, 255, 0);
+            colormanager["blue"] = new Color(0, 0, 255);
+
+            // User adds personalized colors
+            colormanager["angry"] = new Color(255, 54, 0);
+            colormanager["peace"] = new Color(128, 211, 128);
+            colormanager["flame"] = new Color(211, 34, 20);
+
+            // User clones selected colors
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
+        }
+    }
+    //原型类，声明接口，
+    public abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+    //具体实现类
+    public class Color : ColorPrototype
+    {
+        private int _red;
+        private int _green;
+        private int _blue;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Color(int red, int green, int blue)
+        {
+            this._red = red;
+            this._green = green;
+            this._blue = blue;
+        }
+
+        /// <summary>
+        /// Create a shallow copy
+        /// </summary>
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine("Cloning color RGB: {0,3},{1,3},{2,3}", _red, _green, _blue);
+            return this.MemberwiseClone() as ColorPrototype;
+        }
+    }
+    public class ColorManager
+    {
+        private Dictionary<string, ColorPrototype> _colors = new Dictionary<string, ColorPrototype>();
+
+        /// <summary>
+        /// Indexer
+        /// </summary>
+        public ColorPrototype this[string key]
+        {
+            get { return _colors[key]; }
+            set { _colors.Add(key, value); }
+        }
+    }
+
+}
