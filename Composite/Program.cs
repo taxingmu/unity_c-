76ad93cfc9e3325e96b14dea6703407b8274c96c
taxingmu <664300829@qq,com>
2019-04-28 20:31:29 +0800

    组合模式：模糊简单与复杂元素，使单个和组合具有一致性

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -11,38 +11,111 @@ namespace Composite
     //使得用户对单个对象和组合对象的使用具有一致性。
     class Program
     {
+        //使用公司类抽象，总公司和分公司等都抽象
         static void Main(string[] args)
         {
+            ConcreteCompany root = new ConcreteCompany("北京总共司");
+            root.Add(new HRdepartment("人力部"));
+            root.Add(new FinanceDepartment("财务部"));
+            ConcreteCompany comp = new ConcreteCompany("上海分公司");
+            comp.Add(new HRdepartment("分工司人力部"));
+            comp.Add(new FinanceDepartment("分公司财务部"));
+            root.Add(comp);
+            ConcreteCompany comp1 = new ConcreteCompany("南京分部");
+            comp1.Add(new HRdepartment("南京人力部"));
+            comp1.Add(new FinanceDepartment("南京财务部"));
+            comp.Add(comp1);
+            ConcreteCompany comp2 = new ConcreteCompany("杭洲分部");
+            comp2.Add(new HRdepartment("杭州人事部"));
+            comp2.Add(new FinanceDepartment("杭州财务部"));
+            comp.Add(comp2);
+            root.Display(1);
+            Console.ReadKey();
         }
     }
-    public abstract class Graphics
+    //抽象公司类
+    public abstract class Company
     {
-        protected string _name;
+        protected string name;
+        public Company(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Add(Company c);
+        public abstract void Remove(Company c);
+        public abstract void Display(int depth);
+        public abstract void LineOfDuty();
+    }
+    //抽象的公司具体操作类
+    public class ConcreteCompany : Company
+    {
+        private List<Company> children = new List<Company>();
+        public ConcreteCompany(string name)
+          : base(name)
+        { }
+        public override void Add(Company c)
+        {
+            children.Add(c);
+        }
+        public override void Remove(Company c)
+        {
+            children.Remove(c);
+        }
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+            foreach (Company component in children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+        public override void LineOfDuty()
+        {
+            foreach (Company component in children)
+            {
+                component.LineOfDuty();
+            }
+        }
+    }
+    //财务部
+    public class FinanceDepartment : Company
+    {
+        public FinanceDepartment(string name) : base(name) { }
+        public override void Add(Company c)
+        {
+        }
+        public override void Remove(Company c)
+        {
 
-        public Graphics(string name)
-        {
-            _name = name;
         }
-        public abstract void Draw();
-    }
-    public class Picture : Graphics
-    {
-        public Picture(string name) : base(name) { }
-        public override void Draw()
+        public override void Display(int depth)
         {
-            
+            Console.WriteLine(new String('-', depth) + name);
         }
-        public ArrayList GetChild()
+        public override void LineOfDuty()
         {
-
+            Console.WriteLine("{0} 财务支付管理", name);
         }
     }
-    public class Line : Graphics
+    //人事部
+    public class HRdepartment : Company
     {
-        public Line(string name) : base(name) { }
-        public override void Draw()
+        public HRdepartment(string name)
+          : base(name)
+        { }
+        public override void Add(Company c)
         {
-            Console.WriteLine("draw" + _name.ToString());
+        }
+        public override void Remove(Company c)
+        {
+        }
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+        public override void LineOfDuty()
+        {
+            Console.WriteLine("{0} 招聘培训管理", name);
         }
     }
 }
