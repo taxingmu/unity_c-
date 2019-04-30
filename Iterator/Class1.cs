a54e8431db90416c1498fd05963ecc7c7c1cf6ff
taxingmu <664300829@qq,com>
2019-04-30 15:41:14 +0800

    为自定义类实现迭代，方便按顺序获取对象，适用于对象池

---
+++ Iterator/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Class1
+    {
+    }
+    class Fruit : IEnumerable
+    {
+        public List<string> items = new List<string>();
+
+        public Fruit()
+        {
+            items = null;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new  FruitItor();
+        }
+    }
+    class FruitItor : IEnumerator
+    {
+        
+        public  FruitItor()
+        {
+            this;
+        }
+
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
