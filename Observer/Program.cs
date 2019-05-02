d623ef4ecf945618ba03c8f4e9abc8244a6497fd
taxingmu <664300829@qq.com>
2019-05-03 00:44:32 +0800

    观察者模式 IEnum

---
+++ Observer/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    //subject-抽象的主题，被观察的对象
+    //concreatersubject，具体的观察对象
+    //observer抽象的观察者
+    //concreateobserver具体的实现
+    //net的ienumable就是类似于观察者模式
+    class Program
+    {
+        static void Main(string[] args)
+        {// Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            // Change subject and notify observers
+            s.SubjectState = "ABC";
+            s.Notify();
+        }
+        
+    }
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+    public class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in _observers)
+            {
+                o.Update();
+            }
+        }
+    }
+    public class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        /// <summary>
+        /// Gets or sets subject state
+        /// </summary>
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+    public class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            this._subject = subject;
+            this._name = name;
+        }
+
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}", _name, _observerState);
+        }
+
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+}
