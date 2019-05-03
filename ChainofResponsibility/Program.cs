ebc8b23ce84f7a72f0ad1509d21b3bc47812fae2
taxingmu <664300829@qq,com>
2019-05-03 17:28:18 +0800

    职责链模式，解耦发送者请求的和接受者，比如数据进来处理，判断是否进行下一个方法，直到跳出链。对一个产品加工到符合要求

---
+++ ChainofResponsibility/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainofResponsibility
+{
+    //解耦请求发送者与接受者
+    //就像一个产品要经过多道加工程序一样
+        // 采购请求
+        //抽象一个处理过程，如果怎样就传给上一级执行，一环套一环，直到跳出这个链
+        public class PurchaseRequest
+        {
+            // 金额
+            public double Amount { get; set; }
+            // 产品名字
+            public string ProductName { get; set; }
+            public PurchaseRequest(double amount, string productName)
+            {
+                Amount = amount;
+                ProductName = productName;
+            }
+        }
+
+        // 审批人,Handler
+        public abstract class Approver
+        {
+            public Approver NextApprover { get; set; }
+            public string Name { get; set; }
+            public Approver(string name)
+            {
+                this.Name = name;
+            }
+            public abstract void ProcessRequest(PurchaseRequest request);
+        }
+
+        // ConcreteHandler
+        public class Manager : Approver
+        {
+            public Manager(string name)
+                : base(name)
+            { }
+
+            public override void ProcessRequest(PurchaseRequest request)
+            {
+                if (request.Amount < 10000.0)
+                {
+                    Console.WriteLine("{0}-{1} approved the request of purshing {2}", this, Name, request.ProductName);
+                }
+                else if (NextApprover != null)
+                {
+                    NextApprover.ProcessRequest(request);
+                }
+            }
+        }
+
+        // ConcreteHandler,副总
+        public class VicePresident : Approver
+        {
+            public VicePresident(string name)
+                : base(name)
+            {
+            }
+            public override void ProcessRequest(PurchaseRequest request)
+            {
+                if (request.Amount < 25000.0)
+                {
+                    Console.WriteLine("{0}-{1} approved the request of purshing {2}", this, Name, request.ProductName);
+                }
+                else if (NextApprover != null)
+                {
+                    NextApprover.ProcessRequest(request);
+                }
+            }
+        }
+
+        // ConcreteHandler，总经理
+        public class President : Approver
+        {
+            public President(string name)
+                : base(name)
+            { }
+            public override void ProcessRequest(PurchaseRequest request)
+            {
+                if (request.Amount < 100000.0)
+                {
+                    Console.WriteLine("{0}-{1} approved the request of purshing {2}", this, Name, request.ProductName);
+                }
+                else
+                {
+                    Console.WriteLine("Request需要组织一个会议讨论");
+                }
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                PurchaseRequest requestTelphone = new PurchaseRequest(4000.0, "Telphone");
+                PurchaseRequest requestSoftware = new PurchaseRequest(10000.0, "Visual Studio");
+                PurchaseRequest requestComputers = new PurchaseRequest(40000.0, "Computers");
+
+                Approver manager = new Manager("LearningHard");
+                Approver Vp = new VicePresident("Tony");
+                Approver Pre = new President("BossTom");
+
+                // 设置责任链
+                manager.NextApprover = Vp;
+                Vp.NextApprover = Pre;
+
+                // 处理请求
+                manager.ProcessRequest(requestTelphone);
+                manager.ProcessRequest(requestSoftware);
+                manager.ProcessRequest(requestComputers);
+                Console.ReadLine();
+            }
+        }
+    
+}
